7f729b9667ed863b403c1ef8b3681496869ff4b8
renan <renanmontanopaz@outlook.com>
2022-05-13 18:50:42 -0300

    Hora do dia

---
+++ script.js
@@ -0,0 +1,23 @@
+function carregar(){
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    var minutos = data.getMinutes();
+
+    msg.innerHTML = `Agora são ${hora}:${minutos} horas !`
+    if (hora >= 0 && hora < 12){
+        //bom dia
+        img.src = 'img/manhã2.png'
+        document.body.style.background='#F3CC9E'
+    }else if(hora >= 12 && hora <= 18){
+        //boa tarde
+        img.src = 'img/tarde2.png'
+        document.body.style.background='#E59177'
+    }else{
+        //boa noite
+        img.src = 'img/noite2.png'
+        document.body.style.background='#303030'
+    }
+}
+
